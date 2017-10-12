9bdbd24b0ef6b638cd4fadb7e5608238166a8941
dnovaes <novaesdigo@gmail.com>
2017-10-12 02:20:27 -0300

    added normal click and click listener to all edit text fields.

--- app/src/main/java/dnovaes/arenashimizuapp/activities/ScoreBoardActivity.java
+++ app/src/main/java/dnovaes/arenashimizuapp/activities/ScoreBoardActivity.java
@@ -1,5 +1,6 @@
 package dnovaes.arenashimizuapp.activities;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
@@ -9,7 +10,10 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,41 +30,85 @@ import dnovaes.arenashimizuapp.interfaces.IntroPageTransformer;
 public class ScoreBoardActivity extends AppCompatActivity{
 
     private IntroAdapter adapter;
-    private GestureDetector mDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.table_layout);
 
-        //Set an adapter on the viewPager. = FragmentPagerAdapter
-
-        final EditText h1 = (EditText) findViewById(R.id.h1);
-
-        h1.setOnTouchListener(new View.OnTouchListener() {
+        Button bt = (Button)findViewById(R.id.bt_update);
+        bt.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(event.getAction() == MotionEvent.ACTION_DOWN)
-                    handler.postDelayed(mLongPressed, 1000);
-                if (event.getAction() == MotionEvent.ACTION_UP ) {
-                    //Toast.makeText(ScoreBoardActivity.this, "você clicou em ", Toast.LENGTH_SHORT).show();
-                    Integer score = Integer.valueOf(h1.getText().toString());
-                    score++;
-                    h1.setText(score.toString(), TextView.BufferType.EDITABLE);
-                    handler.removeCallbacks(mLongPressed);
-                    return true;
-                }
-                return false;
+            public void onClick(View view) {
+                Intent intent = getIntent();
+                finish();
+                startActivity(intent);
+                //ScoreBoardActivity.this.recreate();
             }
         });
-    }
 
-    final Handler handler = new Handler();
-    Runnable mLongPressed = new Runnable() {
-        public void run() {
-            Toast.makeText(ScoreBoardActivity.this, "LONG PRESS!", Toast.LENGTH_SHORT).show();
+        TableLayout tableSb = (TableLayout) findViewById(R.id.scoreboardLayout);
+
+        for(int rowIndex = 0; rowIndex<tableSb.getChildCount()-1; rowIndex++){
+
+            TableRow row = (TableRow) tableSb.getChildAt(rowIndex);
+
+            for (int i = 1; i < row.getChildCount()-1; i++) {
+
+                View v = row.getChildAt(i);
+                if (v instanceof EditText){
+                    //validate your EditText here
+                    v.setOnClickListener(new View.OnClickListener(){
+
+                        @Override
+                        public void onClick(View view) {
+                            EditText editText = (EditText)view;
+                            Integer score = Integer.valueOf(editText.getText().toString());
+                            score++;
+                            editText.setText(score.toString(), TextView.BufferType.EDITABLE);
+
+                            TableRow row = (TableRow) view.getParent();
+                            if(row.getId() == R.id.border_row3){
+                                EditText et = (EditText)findViewById(R.id.total_guest);
+                                Integer guestScore = Integer.valueOf(et.getText().toString())+1;
+                                et.setText(guestScore.toString(), TextView.BufferType.EDITABLE);
+                            }else if(row.getId() == R.id.border_row4){
+                                EditText et = (EditText)findViewById(R.id.total_home);
+                                Integer homeScore = Integer.valueOf(et.getText().toString())+1;
+                                et.setText(homeScore.toString(), TextView.BufferType.EDITABLE);
+                            }
+                        }
+                    });
+
+                    v.setOnLongClickListener(new View.OnLongClickListener(){
+
+                        @Override
+                        public boolean onLongClick(View view) {
+                            EditText editText = (EditText)view;
+                            Integer previousScore = Integer.valueOf(editText.getText().toString());
+                            Integer score = Integer.valueOf(0);
+                            editText.setText(score.toString(), TextView.BufferType.EDITABLE);
+                            //Toast.makeText(ScoreBoardActivity.this, "LONG PRESS!", Toast.LENGTH_SHORT).show();
+
+                            TableRow row = (TableRow) view.getParent();
+                            if(row.getId() == R.id.border_row3){
+                                EditText et = (EditText)findViewById(R.id.total_guest);
+                                Integer guestScore = Integer.valueOf(et.getText().toString())-previousScore;
+                                et.setText(guestScore.toString(), TextView.BufferType.EDITABLE);
+                            }else if(row.getId() == R.id.border_row4){
+                                EditText et = (EditText)findViewById(R.id.total_home);
+                                Integer homeScore = Integer.valueOf(et.getText().toString())-previousScore;
+                                et.setText(homeScore.toString(), TextView.BufferType.EDITABLE);
+                            }
+                            return true;
+                        }
+                    });
+                }
+            }
         }
-    };
+
+
+    }
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
